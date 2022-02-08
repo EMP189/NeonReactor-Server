85d431ba0a068f9bdc71b5215074017aa59d567e
Hayden Kidd <hkidd@live.co.uk>
2022-02-08 15:47:38 +0000

    added apito start-game

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const io = require("socket.io")(server, {
   }
 })
 const port = process.env.PORT || 8080;
+const axios = require('axios');
 
 server.listen(port, () => console.log(`Listening at http://localhost:${port}`));
 
@@ -15,8 +16,10 @@ io.on('connection', socket => {
     socket.to(room).emit('user-join', username);
   });
 
-  socket.on('start-game', (room, quiz) => {
-    socket.to(room).emit('init-game', quiz);
+  socket.on('start-game', (room, amount, category, difficulty) => {
+    axios.get(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`)
+    .then(response => socket.in(room).emit('init-game', response))
+    .catch(error => socket.in(room).emit('init-error', error));
   });
 
   socket.on('end-game', (room, username, score) => {
